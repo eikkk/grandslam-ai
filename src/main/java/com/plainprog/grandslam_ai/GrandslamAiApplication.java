49f7f0fe2a8fe3df51195ce356b3991901292446
Petro Darchyn <petrodarchyn@gmail.com>
2025-03-05 19:05:22 +0100

    initial commit dfssfsdf

---
+++ src/main/java/com/plainprog/grandslam_ai/GrandslamAiApplication.java
@@ -0,0 +1,14 @@
+package com.plainprog.grandslam_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GrandslamAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandslamAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plainprog/grandslam_ai/GrandslamAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plainprog.grandslam_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandslamAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
