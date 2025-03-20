98101bb50421b57e1bc7747bf4cd1264e4bf8918
Petro Darchyn <petrodarchyn@gmail.com>
2025-03-20 17:53:40 +0100

    init: cleaned auth code

--- src/main/java/com/plainprog/grandslam_ai/controllers/AuthenticationController.java
+++ src/main/java/com/plainprog/grandslam_ai/controllers/AuthenticationController.java
@@ -1,23 +1,15 @@
 package com.plainprog.grandslam_ai.controllers;
 
-import com.plainprog.grandslam_ai.bean.RedisSessionExplorer;
 import com.plainprog.grandslam_ai.model.db.TestTable;
-import com.plainprog.grandslam_ai.repository.TestRepository;
-import com.plainprog.grandslam_ai.service.TestService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,15 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
-
-//    private final HttpSession httpSession;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public AuthenticationController(HttpSession httpSession,
-//                                    PasswordEncoder passwordEncoder) {
-//        this.httpSession = httpSession;
-//        this.passwordEncoder = passwordEncoder;
-//    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login( HttpSession session) {
--- src/main/java/com/plainprog/grandslam_ai/controllers/TestController.java
+++ src/main/java/com/plainprog/grandslam_ai/controllers/TestController.java
@@ -1,39 +1,31 @@
 package com.plainprog.grandslam_ai.controllers;
 
-import com.plainprog.grandslam_ai.bean.RedisSessionExplorer;
 import com.plainprog.grandslam_ai.model.db.TestTable;
-import com.plainprog.grandslam_ai.repository.TestRepository;
 import com.plainprog.grandslam_ai.service.TestService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
 
     @Autowired
     TestService testService;
 
-    @Autowired
-    RedisSerializer<Object> serializer;
-
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name, Principal principal, HttpSession session) throws Exception {
         TestTable obj = testService.getTestTable();
-        System.out.println(principal.getName());
-        String sessionKey = "spring:session:sessions:" + session.getId();
-        RedisSessionExplorer explorer = new RedisSessionExplorer(serializer);
-        explorer.inspectSession(sessionKey);
-        return String.format("Hello %s!", obj.getStr());
+        System.out.println(obj.getStr());
+        return String.format("Hello %s!", principal == null ? "anonymous" : principal.getName());
     }
     @GetMapping("/anonymous-endpoint")
     public String anonymousAccess() {
-//        RedisSessionExplorer.inspectSession("spring:session:sessions:223b0ebe-cd64-4746-a9e8-97fd373c543c");
         return "This is an anonymous endpoint!";
     }
 }
